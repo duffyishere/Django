55b641bff6d1e1eb0046cf8826df7694002745e4
zzunho9 <jamlove4763@naver.com>
2021-07-20 15:34:39 +0900

    3번쩨 커밋, 게시물 수정&삭제 추가

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,3 +1,5 @@
+import blank as blank
+import null as null
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.timezone import now
@@ -7,6 +9,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField(default=now)
+    modify_date = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -16,6 +19,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField(default=now)
+    modify_date = models.DateTimeField(null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
+from django.contrib import messages
 
 from pybo.forms import QuestionForm, AnswerForm
 from pybo.models import Question, Answer
@@ -31,6 +32,7 @@ def answer_create(request, question_id):
 
     if request.method == 'POST':
         form = AnswerForm(request.POST)
+
         if form.is_valid():
             answer = form.save(commit=False)
             answer.create_date = timezone.now();
@@ -54,3 +56,32 @@ def question_create(request):
     else:
         form = QuestionForm()
     return render(request, 'pybo/question_form.html', {'form': form})
+
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
+
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question_id)
+    question.delete()
+    return redirect('pybo:index')
